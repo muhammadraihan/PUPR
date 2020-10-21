604bcb35b3b248016aa442ad3b09481e7fbf4a65
gh0st <ghostofcordelia@protonmail.com>
2020-10-21 00:33:01 +0000

    chore: Adding progres fisik controller resource

---
+++ app/Http/Controllers/ProgresFisikController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ProgresFisik;
+use Illuminate\Http\Request;
+
+class ProgresFisikController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ProgresFisik  $progresFisik
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProgresFisik $progresFisik)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ProgresFisik  $progresFisik
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProgresFisik $progresFisik)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ProgresFisik  $progresFisik
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProgresFisik $progresFisik)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ProgresFisik  $progresFisik
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProgresFisik $progresFisik)
+    {
+        //
+    }
+}
