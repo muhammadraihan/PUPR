f84a5f34cbf57b92974d29ae624b50e1daa9bbd4
gh0st <ghostofcordelia@protonmail.com>
2020-10-24 00:03:36 +0000

    feat: Move fisiks table to pelaksanaans so it provide pelaksanaan keuangan

---
+++ database/migrations/2020_10_23_213232_create_pelaksanaans_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePelaksanaansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pelaksanaans', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid')->unique();
+            $table->bigInteger('pekerjaan_id')->nullable();
+            $table->bigInteger('nomor_pelaksanaan')->nullable();
+            $table->date('tanggal_pelaksanaan')->nullable();
+            $table->float('rencana_fisik',8,2)->nullable();
+            $table->float('realisasi_fisik',8,2)->nullable();
+            $table->float('deviasi_fisik',8,2)->nullable();
+            $table->float('rencana_keuangan',8,2)->nullable();
+            $table->float('realisasi_keuangan',8,2)->nullable();
+            $table->float('deviasi_keuangan',8,2)->nullable();
+            $table->text('permasalahan')->nullable();
+            $table->text('tindakan')->nullable();
+            $table->string('created_by')->nullable();
+            $table->string('edited_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pelaksanaans');
+    }
+}
